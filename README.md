be6af008361167591c4534fadde3555dda59e840
Vaibhav Mali <vaibhavmali17@gmail.com>
2026-08-28 14:42:29 +0530

    add sum of digit number program

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ My daily Java Codes
 1.  Palindrome Number Program in Java
 2.  largest Number Program in Java
 3.  Reverse Number Program in Java
+4.  Sum Of Digits Program in Java
---
+++ SumOfDigits.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class SumOfDigits{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int t = Math.abs(n);
+
+        while (t > 0) {
+            sum += t % 10;
+            t /= 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+        sc.close();
+    }
+}
