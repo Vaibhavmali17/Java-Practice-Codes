5b5c561ee961ff40f440091e8e6ab9a6b1a7229b
Vaibhav Mali <vaibhavmali17@gmail.com>
2026-08-29 15:42:52 +0530

    add swap two numbers without using thrid variable

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ My daily Java Codes
 3.  Reverse Number Program in Java
 4.  Sum Of Digits Program in Java
 5.  Armstrong Number Program in Java
+6.  SwapTwoNumbers program
---
+++ SwapTwoNumbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number (a): ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number (b): ");
+        int b = sc.nextInt();
+
+        System.out.println("Before Swap: a = " + a + ", b = " + b);
+
+        // Swap logic without temp/thrid variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+        sc.close();
+    }
+}
