feca14725e81c142a68e72c2bcde5905e743db95
Vaibhav Mali <vaibhavmali17@gmail.com>
2026-08-30 14:10:03 +0530

    update the programs

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -3,10 +3,13 @@ import java.util.Scanner;
 public class SwapTwoNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter first number (a): ");
         int a = sc.nextInt();
+
         System.out.print("Enter second number (b): ");
         int b = sc.nextInt();
+
 
         System.out.println("Before Swap: a = " + a + ", b = " + b);
 
@@ -14,7 +17,7 @@ public class SwapTwoNumbers {
         a = a + b;
         b = a - b;
         a = a - b;
-
+        
         System.out.println("After Swap: a = " + a + ", b = " + b);
         sc.close();
     }
