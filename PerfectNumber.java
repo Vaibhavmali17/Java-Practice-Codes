293680c1f81c25d756b2e5a7c3781d205e68af5b
Vaibhav Mali <vaibhavmali1712@gmail.com>
2026-09-03 10:53:10 +0530

    Add PerfectNumber class to check perfect numbers
    This program checks if a given number is a perfect number by summing its divisors.

---
+++ PerfectNumber.java
@@ -0,0 +1,27 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        // Find divisors and sum them up
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == n && n > 0) {
+            System.out.println(n + " is a Perfect Number");
+        } else {
+            System.out.println(n + " is NOT a Perfect Number");
+        }
+        sc.close();
+    }
+}
