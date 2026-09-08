2992035da5c599f5e4dc1af1dda927dee116eb34
Vaibhav Mali <vaibhavmali1712@gmail.com>
2026-09-08 09:27:56 +0530

    Add SpyNumber class to check for Spy Numbers
    Sum of digits and equals product of digits,
    e.g., No= 1124   
      1+1+2+4 = 8 and
       1*1* 2*4 = 8

---
+++ SpyNumber.java
@@ -0,0 +1,29 @@
+package Numbers;
+
+import java.util.Scanner;
+
+public class SpyNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+        int product = 1;
+        int temp = Math.abs(num);
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit;
+            product *= digit;
+            temp /= 10;
+        }
+
+        if (sum == product) {
+            System.out.println(num + " is a Spy Number");
+        } else {
+            System.out.println(num + " is NOT a Spy Number");
+        }
+        sc.close();
+    }
+}
