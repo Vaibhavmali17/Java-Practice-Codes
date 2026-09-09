0f7b675eb0a34fa03b041f391a27ef9ad869d6dc
Vaibhav Mali <vaibhavmali1712@gmail.com>
2026-09-09 10:07:57 +0530

    Fix word count logic in CountWords.java

---
+++ CountWords.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CountWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+
+      
+        String str = sc.nextLine().trim();
+
+        if (str.isEmpty()) {
+            System.out.println("Total words: 0");
+        } else {
+
+
+
+          
+            // Space se split karke direct length le lo
+            String[] words = str.split("\\s+");
+            System.out.println("Total words: " + words.length);
+        }
+
+        sc.close();
+    }
+}
