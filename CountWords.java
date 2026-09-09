0245bd7767ff04fb10be374f548921bd7f8d169c
Vaibhavmali17 <vaibhavmali1712@gmail.com>
2026-09-09 10:10:05 +0530

    Add a count word program

--- CountWords.java
+++ CountWords.java
@@ -4,17 +4,11 @@ public class CountWords {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
-
-      
         String str = sc.nextLine().trim();
 
         if (str.isEmpty()) {
             System.out.println("Total words: 0");
-        } else {
-
-
-
-          
+        } else {          
             // Space se split karke direct length le lo
             String[] words = str.split("\\s+");
             System.out.println("Total words: " + words.length);
