a4b578682410c81ec8810456035a222b0e9b944f
Vaibhav Mali <vaibhavmali1712@gmail.com>
2026-09-09 10:11:57 +0530

    Remove space in string from RemoveSpaces.java
    Removed space replacement logic and output statements.

---
+++ RemoveSpaces.java
@@ -0,0 +1,14 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string with spaces: ");
+        String str = sc.nextLine();
+
+        // replace() se saare spaces blank se replace ho jayenge
+        
+    }
+}
