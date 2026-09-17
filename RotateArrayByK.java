315ffe33032ec8c1686756d868f7f97a3a65d540
Vaibhavmali17 <vaibhavmali1712@gmail.com>
2026-09-17 10:43:40 +0530

    update the programs

--- RotateArrayByK.java
+++ RotateArrayByK.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class RotateArrayByK {
     public static void main(String[] args) {
+
         int[] arr = {1, 2, 3, 4, 5};
         int k = 2; // Kitne steps se right rotate karna hai
 
@@ -12,11 +13,13 @@ public class RotateArrayByK {
         // Result array
         int[] rotated = new int[n];
 
+
         for (int i = 0; i < n; i++) {
             rotated[(i + k) % n] = arr[i];
         }
 
         System.out.println("Original: " + Arrays.toString(arr));
+        
         System.out.println("Rotated by " + k + " steps: " + Arrays.toString(rotated));
     }
 }
