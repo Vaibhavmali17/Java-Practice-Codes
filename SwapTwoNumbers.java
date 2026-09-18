5314902394aec8438a80de7db5defdc9ddb62fd9
Vaibhavmali17 <vaibhavmali1712@gmail.com>
2026-09-18 23:27:59 +0530

    update the java programs of swap two  number

--- SwapTwoNumbers.java
+++ SwapTwoNumbers.java
@@ -7,13 +7,14 @@ public class SwapTwoNumbers {
 
         System.out.print("Enter second number (b): ");
         int b = sc.nextInt();
-
-        System.out.println("Before Swap: a = " + a + ", b = " + b);
+          System.out.println("Before Swap: a = " + a + ", b = " + b);
         // Swap logic without temp/thrid variable
+        
         a = a + b;
         b = a - b;
         a = a - b;
-        
+
+ 
         System.out.println("After Swap: a = " + a + ", b = " + b);
         sc.close();
     }
